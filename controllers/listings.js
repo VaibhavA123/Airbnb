fab9158dac5610df7ae07c0eeb149cbdc5908d03
Vaibhavgoel <vaibhavgoel474@gmail.com>
2024-06-20 12:00:23 +0530

    new files

--- controllers/listings.js
+++ controllers/listings.js
@@ -26,11 +26,13 @@ module.exports.showListing = async (req,res) => {
 
 module.exports.createListing = async (req,res,next) => {
     let url = req.file.path;
+    let { placeType } = req.body.placeType;
     let filename = req.file.filename;
     console.log(url,"...",filename);
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
     newListing.image = {url,filename};
+    newListing.placeType = placeType;
     await newListing.save();
     req.flash("success","New Listing created!");
     res.redirect("/listings");
